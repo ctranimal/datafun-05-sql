2dd57c39636fba6f64c44634a65b3155b71939db
Ken McTran <canh.ken.tran@gmail.com>
2025-07-30 15:05:35 -0400

    Changeed db03_queries.py to handle queries

--- db03_queries.py
+++ db03_queries.py
@@ -10,6 +10,7 @@ Use Python to execute feature queries from the sql_features folder.
 import sqlite3
 import os
 import pathlib
+import argparse   ## to parse argument for command-line 
 
 # Import local modules
 from utils_logger import logger
@@ -55,6 +56,17 @@ def main() -> None:
     logger.info(f"Global vars ROOT_DIR: {utils_project.ROOT_DIR}")
     logger.info(f"Global vars DB_PATH: {utils_project.DB_PATH}")  
 
+    parser = argparse.ArgumentParser(description="A simple script with arguments.")
+    parser.add_argument("--setup", type=str, help="enter a number")
+    parser.add_argument("--query", type=str, help="enter a number")
+
+    args = parser.parse_args()
+
+    if args.setup:
+        logger.info(f"Program is called with argument -setup {args.setup}.")
+    if args.query:
+        logger.info(f"Program is called with argument -query {args.query}.")
+
     # Ensure the database file exists before attempting to connect
     if not utils_project.DB_PATH.exists():
         logger.error(f"Database file not found at {utils_project.DB_PATH}. Ensure the database is created first.")
@@ -68,7 +80,31 @@ def main() -> None:
         # Execute all SQL files in the sql_features folder
         #for sql_file in sorted(utils_project.SQL_SCRIPT_FOLDER.glob("*.sql")):
         #    execute_sql_file(connection, sql_file)
-        execute_sql_file(connection, utils_project.SQL_SCRIPT_FOLDER.joinpath('dcase_sample_query01.sql'))
+
+        if(args.setup): 
+            if(int(args.setup) == 1): 
+                utils_project.set_globalvars_for_project_folders("sql_create") 
+                execute_sql_file(connection, utils_project.SQL_SCRIPT_FOLDER.joinpath('01_drop_tables.sql'))
+                execute_sql_file(connection, utils_project.SQL_SCRIPT_FOLDER.joinpath('02_create_tables.sql'))
+            else:
+                logger.error(f"Unknown option -setup ={args.setup}")        
+
+        if(args.query):
+            utils_project.set_globalvars_for_project_folders("sql_queries") 
+            if(int(args.query) == 1): 
+                execute_sql_file(connection, utils_project.SQL_SCRIPT_FOLDER.joinpath('01_query_aggregation.sql'))
+            elif(int(args.query) == 2):
+                execute_sql_file(connection, utils_project.SQL_SCRIPT_FOLDER.joinpath('02_query_filter.sql'))
+            elif(int(args.query) == 3):
+                execute_sql_file(connection, utils_project.SQL_SCRIPT_FOLDER.joinpath('03_query_group_by.sql')) 
+            elif(int(args.query) == 4):
+                execute_sql_file(connection, utils_project.SQL_SCRIPT_FOLDER.joinpath('04_query_join.sql'))
+            elif(int(args.query) == 5):
+                execute_sql_file(connection, utils_project.SQL_SCRIPT_FOLDER.joinpath('05_query_sorting.sql'))  
+            else:
+                logger.error(f"Unknown option -query ={args.query}")
+
+
 
         logger.info("Feature queries execution completed successfully.")
     except Exception as e:
