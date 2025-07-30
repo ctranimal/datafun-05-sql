ca947978c5a9c7af96556975ecaa15fc3343a0b9
Ken McTran <canh.ken.tran@gmail.com>
2025-07-30 09:01:01 -0400

    Documented db02_features_001.py to add a column to Students table

---
+++ sql_features/add_to_students_yearborn.sql
@@ -0,0 +1,9 @@
+-- Add a new column to the books table to store the author's age at the time of publication 
+ALTER TABLE Students ADD COLUMN year_born INT;
+
+-- Update the author_age_at_publication column in the Students table
+-- NOTE: You might need to refresh the database to see results in the Students table
+UPDATE Students
+SET year_born = 2010
+    WHERE Students.last_name = "Doe"; -- Add all of Doe's year_born to 2010;
+    
